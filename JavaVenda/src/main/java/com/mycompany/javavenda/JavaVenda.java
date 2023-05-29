c0e6dd3f7b3b83542565400009cba524bf60e775
piaskowskirodrigo <piaskowskirodrigo@gmail.com>
2023-05-29 14:07:27 -0300

    Create project

---
+++ JavaVenda/src/main/java/com/mycompany/javavenda/JavaVenda.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.javavenda;
+
+/**
+ *
+ * @author piask
+ */
+public class JavaVenda {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
